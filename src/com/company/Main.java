449d405faf573cbeb0b6e740da58b112f73b383c
Brian Calvo Aragón <Brian Calvo Aragn>
2016-09-16 17:04:58 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+
+        Coche jaguar = new Coche("Jaguar","XF","3453WRD",4000,80000.0);
+        Coche mercedes = new Coche("Mercedes","jgjhgj","3234RRD",8000,160000.0);
+        Coche bmw = new Coche("BMW","dfsdas","7645DRE",5000,60000.0);
+
+        List<Coche> cocheList = new ArrayList<>();
+
+        cocheList.add(jaguar);
+        cocheList.add(mercedes);
+        cocheList.add(bmw);
+
+        System.out.println("Muestro el Jaguar: "+cocheList.get(0));
+
+        
+
+
+    }
+}
