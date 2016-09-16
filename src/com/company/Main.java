ffac89e7568e1354aeacc68f2fee3b52ebcf304a
Brian <brian.calvo.aragon@gmail.com>
2016-09-16 17:48:53 +0200

    Añado la consulta del BMW.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -9,9 +10,9 @@ public class Main {
 	// write your code here
         System.out.println("Hello World!");
 
-        Coche jaguar = new Coche("Jaguar","XF","3453WRD",4000,80000.0);
-        Coche mercedes = new Coche("Mercedes","jgjhgj","3234RRD",8000,160000.0);
-        Coche bmw = new Coche("BMW","dfsdas","7645DRE",5000,60000.0);
+        Coche jaguar = new Coche("Jaguar","XF","3453WRD",4000,42943.0);
+        Coche mercedes = new Coche("Mercedes","Coupe","3234RRD",8000,44500.0);
+        Coche bmw = new Coche("BMW","X1","7645DRE",5000,30950.0);
 
         List<Coche> cocheList = new ArrayList<>();
 
@@ -22,6 +23,15 @@ public class Main {
         System.out.println("Muestro el Jaguar: "+cocheList.get(0));
 
         System.out.println("Muestro el Mercedes: "+cocheList.get(1));
+
+        HashMap<String,Coche> bmwMap = new HashMap<String,Coche>();
+
+        bmwMap.put(bmw.getMatricula(),bmw);
+
+        System.out.println("Muestro el BMW: "+bmwMap.get(bmw.getMatricula()));
+
+
+
 
 
 
